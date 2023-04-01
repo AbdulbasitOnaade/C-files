f535acaadfc0c0520e6b5f1b0aebc3e1b73fe924
AbdulbasitOnaade <ethdamilee17@gmail.com>
2023-04-01 11:22:53 +0100

    C self tutorial

--- helloworld.c
+++ helloworld.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 
-int main()
-{
-    int age;
-    printf("\nEnter your age: ");
-    scanf("%d", &age);
-    if (age >= 18)
-    {
-        printf("You are now signed up!");
-    }
-    else if (age < 18)
-    {
-        printf("You are too young to sign up!");
-    }
-    else if (age <= 0)
-    {
-        printf("You are not yet born!");
-    }
-    else
-    {
-        printf("You are too young to sign up!");
-    }
+// int main()
+// {
+//     int age;
+//     printf("\nEnter your age: ");
+//     scanf("%d", &age);
 
-    return 0;
+//     if (age >= 18)
+//     {
+//         printf("You are now signed up!");
+//     }
+//     else if (age < 18) 
+//     {
+//         printf("You are too young to sign up!");
+//     }
+//     else
+//     {
+//         printf("You are not yet born!");
+//     }
+
+//     return 0;
+// }
+
+int main(){
+    int num1 = 12 % 4;
+    int num2 = 180/8;
+    printf("%d", num1);
+    printf("%d", num2);
+
+return 0;
 }
