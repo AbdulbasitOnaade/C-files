396687a22ddc37403b5e14a414bd92948b61e74d
AbdulbasitOnaade <ethdamilee17@gmail.com>
2023-04-02 13:26:16 +0100

    ascii

--- JavaTpoint.c
+++ JavaTpoint.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    char ch;
-    printf("Enter a character: ");
-    scanf("%c", &ch);
-    printf("\nThe ascii value of the ch variable is: %d", ch);
-    return 0;
+    // char ch;
+    // printf("Enter a character: ");
+    // scanf("%c", &ch);
+    // printf("\nThe ascii value of the ch variable is: %d", ch);
+    // return 0;
     // int k;
     // for(k=0; k<=255; k++)
     // {
@@ -13,4 +13,19 @@ int main()
     // }
 
     // return 0;
+    int sum=0;
+    char name[20];
+    int i=0;
+
+    printf("Enter name: ");
+    scanf("%s", name);
+
+    while (name[i] != '\0')
+    {
+        printf("\nThe ascii value of the character %c is %d", name[i],name[i]);
+        sum=sum+name[i];
+        i++;
+    }
+    printf("\nSum of the ascii value of a string is: %d", sum);
+    return 0;
 }
