7b2deecfa27d82e1a8be88778979685285af220d
AbdulbasitOnaade <ethdamilee17@gmail.com>
2023-04-02 09:38:35 +0100

    java

--- JavaTpoint.c
+++ JavaTpoint.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    // char ch;
-    // printf("Enter a character: ");
-    // scanf("%c", &ch);
-    // printf("\nThe ascii value of the ch variable is: %d", ch);
-    // return 0;
-    int k;
-    for(k=0; k<=255; k++)
-    {
-        printf("\nThe ascii code for %c is %d", k,k);
-    }
-
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    printf("\nThe ascii value of the ch variable is: %d", ch);
     return 0;
+    // int k;
+    // for(k=0; k<=255; k++)
+    // {
+    //     printf("\nThe ascii code for %c is %d", k,k);
+    // }
+
+    // return 0;
 }
