e784ada3c96caf2f6fb3646ff8dbed410e896000
AbdulbasitOnaade <ethdamilee17@gmail.com>
2023-05-08 15:38:17 +0100

    g

--- vscode/JavaTpoint.c
+++ vscode/JavaTpoint.c
@@ -13,21 +13,19 @@ int main()
     // }
 
     // return 0;
-    int sum=0;
+    int sum = 0;
     char name[20];
-    int i=0;
+    int i = 0;
 
     printf("Enter name: ");
     scanf("%s", name);
 
     while (name[i] != '\0')
     {
-        printf("\nThe ascii value of the character %c is %d", name[i],name[i]);
-        sum=sum+name[i];
+        printf("\nThe ascii value of the character %c is %d", name[i], name[i]);
+        sum = sum + name[i];
         i++;
     }
     printf("\nSum of the ascii value of a string is: %d", sum);
     return 0;
-
-
 }
--- vscode/helloworld.c
+++ vscode/helloworld.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
+#include "limits.h"
 
+/*int main()
+{
+    int age;
+    printf("\nEnter your age: ");
+    scanf("%d", &age);
 
-// int main()
-// {
-//     int age;
-//     printf("\nEnter your age: ");
-//     scanf("%d", &age);
+    if (age >= 18)
+    {
+        printf("You are now signed up!");
+    }
+    else if (age < 18)
+    {
+        printf("You are too young to sign up!");
+    }
+    else
+    {
+        printf("You are not yet born!");
+    }
 
-//     if (age >= 18)
-//     {
-//         printf("You are now signed up!");
-//     }
-//     else if (age < 18) 
-//     {
-//         printf("You are too young to sign up!");
-//     }
-//     else
-//     {
-//         printf("You are not yet born!");
-//     }
+    return 0;
+}*/
 
-//     return 0;
-// }
+int main()
+{
+    int age;
+    printf("\nEnter your age: ");
+    scanf("%d", &age);
+
+    switch (age)
+    {
+    case 0:
+        printf("You've just been born.\n");
+        break;
+    case 1 ... 17:
+        printf("You're too young to sign up.\n");
+        break;
+    case 18 ... INT_MAX:
+        printf("You've signed up successfully.\n");
+        break;
+
+    default:
+        printf("Enter a valid age number");
+    }
+}
 
 // int main(){
 //     int num1 = 12 % 4;
@@ -30,7 +53,8 @@
 //     printf("\n%d", num2);
 //     return 0;
 // }
-int main(){
+/*int main()
+{
 int x;
 int y;
 int result;
@@ -39,8 +63,8 @@ scanf("%d", &x);
 
 printf("\nEnter num2:");
 scanf("%d", &y);
-result = x+y;
+result = x + y;
 printf("\nSum of 2 numbers: %d", result);
 
 return 0;
-}
+}*/
