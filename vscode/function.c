8e6f2558b2bcc571be6e5909612934ae373f1571
AbdulbasitOnaade <ethdamilee17@gmail.com>
2023-05-22 11:06:31 +0100

    gj

--- vscode/function.c
+++ vscode/function.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 
-void greetings(char name[], double time)
-{
-    printf("\nHELLO Everyone!\n");
-    printf("\nMy name is %s!", name);
-    printf("\nI arrived at exactly %.2lf am", time);
-}
 
+
+// void greetings(char name[], double time)
+// {
+//     printf("\nHELLO Everyone!\n");
+//     printf("\nMy name is %s!", name);
+//     printf("\nI arrived at exactly %.2lf am", time);
+// }
+
+// int main()
+// {
+//     char name[] = "Abdulbasit";
+//     double time = 12.00;
+
+//     greetings(name, time);
+// }
+int findMax(int x, int y)
+{
+    if (x > y)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+}
 int main()
 {
-    char name[] = "Abdulbasit";
-    double time = 12.00;
+    int max = findMax(3, 9);
+    printf("%d\n", max);
 
-    greetings(name, time);
+    
+
+    return 0;
 }
