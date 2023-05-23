797a62be2fefb7ee35f3c0034a3f1d7afde58c50
AbdulbasitOnaade <ethdamilee17@gmail.com>
2023-05-23 22:07:42 +0100

    while

--- vscode/for_loop.c
+++ vscode/for_loop.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    for (int i = 1; i <= 10; i++)
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     printf("test\n");
+    //     printf("\n%d", i);
+    // }
+
+    char name[25];
+
+    printf("\nEnter your name: ");
+    fgets(name, 25, stdin);
+    name[strlen(name) - 1] = '\0';
+
+    while (strlen(name) == 0)
     {
-        printf("test\n");
+        printf("\nYou must enter a name.");
+        printf("\nEnter your name:");
+        fgets(name, sizeof(name), stdin);
+        name[strlen(name) - 1] = '\0';
     }
+    printf("\nHello %s", name);
+    return 0;
 }
