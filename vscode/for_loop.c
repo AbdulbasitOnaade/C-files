3c5fa8b1dc7713d20c89ba1411018c3ec6da10dd
AbdulbasitOnaade <ethdamilee17@gmail.com>
2023-05-23 23:10:49 +0100

    do while

--- vscode/for_loop.c
+++ vscode/for_loop.c
@@ -9,19 +9,29 @@ int main()
     //     printf("\n%d", i);
     // }
 
-    char name[25];
+    // char name[25];
 
-    printf("\nEnter your name: ");
-    fgets(name, 25, stdin);
-    name[strlen(name) - 1] = '\0';
+    // printf("\nEnter your name: ");
+    // fgets(name, 25, stdin);
+    // name[strlen(name) - 1] = '\0';
 
-    while (strlen(name) == 0)
+    // while (strlen(name) == 0)
+    // {
+    //     printf("\nYou must enter a name.");
+    //     printf("\nEnter your name:");
+    //     fgets(name, sizeof(name), stdin);
+         
+    // }
+    // printf("\nHello %s", name);
+    // return 0;
+    int number = 1;
+    int sum = 0;
+do{
+    printf("\nEnter a number above 1: ");
+    scanf("%d", &number);
+    if(number > 1)
     {
-        printf("\nYou must enter a name.");
-        printf("\nEnter your name:");
-        fgets(name, sizeof(name), stdin);
-        name[strlen(name) - 1] = '\0';
+        sum += number;
     }
-    printf("\nHello %s", name);
-    return 0;
+}while()
 }
