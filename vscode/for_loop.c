2d9be586b68c3432dcf5705905550edcdd874ce9
AbdulbasitOnaade <ethdamilee17@gmail.com>
2023-05-25 14:59:09 +0100

    do

--- vscode/for_loop.c
+++ vscode/for_loop.c
@@ -41,12 +41,25 @@
 
 int main()
 {
-    int num = 0;
+    // int num = 2;
 
-    while (num <= 20)
+    // while (num <= 20)
+    // {
+    //     printf("%d\n", num);
+    //     num += 2;
+    // }
+    // return 0;
+    int num = 0;
+    int sum = 0;
+    do
     {
-        printf("%d\n", num);
-        num += 2;
-    }
-    return 0;
+        printf("Enter a number above 1: ");
+        scanf("%d", &num);
+        if (num > 1)
+        {
+            sum += num;
+        }
+
+    } while (num > 1);
+    printf("The sum is %d\n", sum);
 }
