2da132b96c3a680120f2e7f7ac95dbed1f238543
AbdulbasitOnaade <ethdamilee17@gmail.com>
2023-05-25 15:01:50 +0100

    test

---
+++ vscode/Test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+ int num = 0;
+    int sum = 0;
+    do
+    {
+        printf("Enter a number above 1: ");
+        scanf("%d", &num);
+        if (num > 1)
+        {
+            sum += num;
+        }
+
+    } while (num > 1);
+    printf("The sum is %d\n", sum);   
+}
